585088bf735bc7a90d0a4009dcc5d0b23195707e
bhavinsen <senbhavin@gmail.com>
2022-11-15 18:15:57 +0530

    created the GET endpoint to display json file data

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get('/chat/recieve')
+def recieve_massages():
+#    return db
+    with open('search.json',mode='r') as myfile:
+        json_data = json.load(myfile)
+        data_info_arr= []
+        for i in json_data:
+            data_arr = []
+            operator = i["operator"]
+            if i.get("operating_manager",None) is not None:
+                operating_manager = i["operating_manager"]
+                data_arr.append(operating_manager)
+            site = i["site"]
+            ordz = i["ordz"]
+            category = i["category"]
+            description = i["description"]
+            next_inspection = i["next_inspection"]
+            data_arr.extend((operator, site, ordz, category, description ,next_inspection))
+            data_info_arr.append(data_arr)
+
+        return data_info_arr
